b8dedb1a6462cf8b1a8020e441f6166eed28d6c0
Baibhab Mondal <vbmandal98@gmail.com>
2018-12-23 18:47:40 +0530

    mongo set up

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const cred = require('./cred')
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlhttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(`mongodb://${cred.user}:${cred.pass}@ds243054.mlab.com:43054/eventgraphql`, {
+  useNewUrlParser: true
+})
+  .then((data) => { console.log("database set up") })
+  .catch(err => { console.log(err) })
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -65,4 +73,5 @@ app.get('/', (req, res, next) => {
     res.status(200).json({msg: 'hey it works'});
 })
 
-app.listen(process.env.PORT || 3000, () => {console.log("listening")});
+const port = process.env.PORT || 3000
+app.listen(port, () => {console.log(`listening to ${port}`)});
